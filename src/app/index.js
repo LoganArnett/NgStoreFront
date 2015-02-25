b005ce160f24355c7e5b13a04f5034e2f5b48495
LoganArnett <Loganarn5@gmail.com>
2015-02-24 20:27:05 -0500

    new states for view change

--- src/app/index.js
+++ src/app/index.js
@@ -16,7 +16,30 @@ angular.module('ngStoreFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'restangula
                     return Restangular.one('app/listings/trending.json').get();
              }
         }
+      })
+      .state('grid', {
+        url: '/grid',
+        templateUrl: 'app/change/product-grid.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        resolve: {
+            trending: function(Restangular){
+                    return Restangular.one('app/listings/trending.json').get();
+             }
+        }
+      })
+      .state('listings', {
+        url: '/listings',
+        templateUrl: 'app/change/product-list.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        resolve: {
+            trending: function(Restangular){
+                    return Restangular.one('app/listings/trending.json').get();
+             }
+        }
       });
+      
 
     $urlRouterProvider.otherwise('/');
   })
